926a138421cd565403714123433f667226a1360d
Mustafa Emir Ata <mustafaemirata@outlook.com>
2026-02-26 13:41:27 +0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Mustafa Emir Ata on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankAppTests/BankAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppTests.swift
+//  BankAppTests
+//
+//  Created by Mustafa Emir Ata on 26.02.2026.
+//
+
+import Testing
+@testable import BankApp
+
+struct BankAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
