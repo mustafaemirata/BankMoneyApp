dd3ad926d3dbe82ff8634db2205ca3dae7fc9ebd
Mustafa Emir Ata <mustafaemirata@outlook.com>
2026-03-04 11:32:52 +0300

    Ayarlarve kullanıcı servisleri geliştirildi.

---
+++ BankApp/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  BankApp
+//
+//  Created by Mustafa Emir Ata on 4.03.2026.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ BankApp/Transfer/View/TransferView.swift
@@ -0,0 +1,18 @@
+//
+//  TransferView.swift
+//  BankApp
+//
+//  Created by Mustafa Emir Ata on 3.03.2026.
+//
+
+import SwiftUI
+
+struct TransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransferView()
+}
