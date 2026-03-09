b130780302ebf39180308cfb5c9d1aacbf6f615e
Mustafa Emir Ata <mustafaemirata@outlook.com>
2026-03-09 13:31:48 +0300

    TAMAMLANDI. Yatırım, altın, gümüş alım satımı ve aktarımları eklendi.

---
+++ BankApp/Investment/View/InvestmentView.swift
@@ -0,0 +1,18 @@
+//
+//  InvestmentView.swift
+//  BankApp
+//
+//  Created by Mustafa Emir Ata on 9.03.2026.
+//
+
+import SwiftUI
+
+struct InvestmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvestmentView()
+}
